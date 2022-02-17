77a924ef30233a402974b63e8a9a2dd8fa1dc035
Jkinjt <l02jitoj@gmail.com>
2022-02-17 14:14:49 +0100

    modelos

---
+++ src/main/java/com/iesfranciscodelosrios/Cajero/server/model/Account.java
@@ -0,0 +1,94 @@
+package com.iesfranciscodelosrios.Cajero.server.model;
+
+import java.util.Objects;
+
+public class Account {
+	private int id;
+	private float balance;
+	private Client client;
+	/**
+	 * @param id
+	 * @param balance
+	 * @param client
+	 */
+	public Account(int id, float balance, Client client) {
+		super();
+		this.id = id;
+		this.balance = balance;
+		this.client = client;
+	}
+	/**
+	 * @param balance
+	 * @param client
+	 */
+	public Account(float balance, Client client) {
+		super();
+		this.balance = balance;
+		this.client = client;
+	}
+	/**
+	 * 
+	 */
+	public Account() {
+		super();
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the balance
+	 */
+	public float getBalance() {
+		return balance;
+	}
+	/**
+	 * @param balance the balance to set
+	 */
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+	/**
+	 * @return the client
+	 */
+	public Client getClient() {
+		return client;
+	}
+	/**
+	 * @param client the client to set
+	 */
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", balance=" + balance + ", client=" + client + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(balance, client, id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Account other = (Account) obj;
+		return Float.floatToIntBits(balance) == Float.floatToIntBits(other.balance)
+				&& Objects.equals(client, other.client) && id == other.id;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/iesfranciscodelosrios/Cajero/server/model/Client.java
@@ -0,0 +1,126 @@
+package com.iesfranciscodelosrios.Cajero.server.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Client {
+	private int id;
+	private String name;
+	private String password;
+	private List<Account> acounts;
+	/**
+	 * @param id
+	 * @param name
+	 * @param password
+	 */
+	public Client(int id, String name, String password, List<Account> accounts) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.acounts=accounts;
+	}
+	/**
+	 * 
+	 */
+	public Client() {
+		super();
+	}
+	/**
+	 * @param name
+	 * @param password
+	 */
+	public Client(String name, String password) {
+		super();
+		this.id=-1;
+		this.name = name;
+		this.password = password;
+		this.acounts=new ArrayList<>();
+	}
+	
+	
+	/**
+	 * @param name
+	 * @param password
+	 * @param acounts
+	 */
+	public Client(String name, String password, List<Account> acounts) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.acounts = acounts;
+	}
+	
+	
+	/**
+	 * @return the acounts
+	 */
+	public List<Account> getAcounts() {
+		return acounts;
+	}
+	/**
+	 * @param acounts the acounts to set
+	 */
+	public void setAcounts(List<Account> acounts) {
+		this.acounts = acounts;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Client [id=" + id + ", name=" + name + ", password=" + password + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return id == other.id;
+	}
+	
+	
+
+}
