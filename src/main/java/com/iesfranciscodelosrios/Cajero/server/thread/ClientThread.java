0cf02d68d7a2ca6baee4ed0aeaefd8895518c0c0
Jkinjt <l02jitoj@gmail.com>
2022-02-25 11:45:58 +0100

    Peticiones del cliente

--- src/main/java/com/iesfranciscodelosrios/Cajero/server/thread/ClientThread.java
+++ src/main/java/com/iesfranciscodelosrios/Cajero/server/thread/ClientThread.java
@@ -1,8 +1,13 @@
 package com.iesfranciscodelosrios.Cajero.server.thread;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 import com.iesfranciscodelosrios.Cajero.server.model.Client;
+
+
 
 public class ClientThread extends Thread {
 	private Socket socket;
@@ -51,11 +56,67 @@ public class ClientThread extends Thread {
 	public void run() {
 		
 		if(this.socket!=null) {
-			
+			while (true) {
+				try {
+					DataInputStream input=new DataInputStream(this.socket.getInputStream());
+					int option=input.readInt();
+					input.close();
+					choseOptionClient(option);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				
+			}
 		}
 		
 		
 	}
+	
+	/**
+	 * Método que obtiene el salario de una cuenta
+	 * @return
+	 */
+	public int getSalary() {
+		
+		return 500;
+	}
+	
+	public int substracSalary(int substracAmount) {
+		
+		return 500-substracAmount;
+	}
+	
+	public int enterSalary(int enterSalary) {
+		
+		return 500+enterSalary;
+	}
+	
+	/**
+	 * Recibe un número que establece que tipo de petición esta haciendo el cliente
+	 */
+	public void choseOptionClient(int option) {
+		switch (option) {
+		case 1:
+			System.out.println("opcion "+ option);
+			break;
+		case 2:
+			System.out.println("opcion "+ option);
+			break;
+		case 3:
+			System.out.println("opcion "+ option);
+	
+	break;	
+
+		default:
+			break;
+		}
+	}
+	
+	
+	
 	
 
 }
